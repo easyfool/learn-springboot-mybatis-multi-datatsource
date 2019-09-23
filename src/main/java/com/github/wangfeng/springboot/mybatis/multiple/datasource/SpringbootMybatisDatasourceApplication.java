dbbfa8802610185be590faec0fd2bd9888b204fe
wangfeng <wangfeng@unionpayintl.com>
2019-09-23 11:20:47 +0800

    fix: mysql 5.7+ hakariCP

--- src/main/java/com/github/wangfeng/springboot/mybatis/multiple/datasource/SpringbootMybatisDatasourceApplication.java
+++ src/main/java/com/github/wangfeng/springboot/mybatis/multiple/datasource/SpringbootMybatisDatasourceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSpringbootMybatisMultiDatasourceApplication {
+public class SpringbootMybatisDatasourceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnSpringbootMybatisMultiDatasourceApplication.class, args);
+        SpringApplication.run(SpringbootMybatisDatasourceApplication.class, args);
     }
 
 }
